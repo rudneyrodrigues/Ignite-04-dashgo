ed97018c69f7fa04735eb389efc3e68273f66a18
Rudney R <rudney.un2016@gmail.com>
2022-05-02 18:44:06 -0300

    Component Sidebar

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,9 +1,21 @@
+import Head from "next/head";
+import { Flex } from '@chakra-ui/react';
+
 import { Header } from "../components/Header";
+import { Sidebar } from "../components/Sidebar";
 
 export default function Dashboard() {
   return (
-    <>
+    <Flex direction="column" h="100vh">
+      <Head>
+        <title>Dashboard | dashgo</title>
+      </Head>
+
       <Header />
-    </>
+
+      <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
+        <Sidebar />
+      </Flex>
+    </Flex>
   )
 }
