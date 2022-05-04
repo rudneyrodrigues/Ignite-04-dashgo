61ec44cbc6afdd9945f82c5b476f5f4eadf04a77
Rudney R <rudney.un2016@gmail.com>
2022-05-04 00:20:35 -0300

    Backup

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,8 +1,57 @@
 import Head from "next/head";
-import { Flex } from '@chakra-ui/react';
+import dynamic from 'next/dynamic';
+import { Box, Flex, SimpleGrid, Text, theme } from '@chakra-ui/react';
+
+const Chart = dynamic(() => import('react-apexcharts'), { ssr: false });
 
 import { Header } from "../components/Header";
 import { Sidebar } from "../components/Sidebar";
+
+const options = {
+  chart: {
+    toolbar: {
+      show: false,
+    },
+    zoom: {
+      enabled: false,
+    },
+    foreColor: theme.colors.gray[500],
+  },
+  grid: {
+    show: false,
+  },
+  dataLabels: {
+    enabled: false,
+  },
+  tooltip: {
+    enabled: false,
+  },
+  xaxis: {
+    type: 'area',
+    axisBorder: {
+      color: theme.colors.gray[600],
+    },
+    axisTicks: {
+      color: theme.colors.gray[600],
+    },
+    categories: [
+      '2022-04-25T00:00:00.000Z',
+      '2022-04-26T00:00:00.000Z',
+      '2022-04-27T00:00:00.000Z',
+      '2022-04-28T00:00:00.000Z',
+      '2022-04-29T00:00:00.000Z',
+      '2022-04-30T00:00:00.000Z',
+    ]
+  }
+};
+
+const series = [
+  {
+    name: 'series1',
+    data: [31, 120, 10, 28, 51, 18, 109]
+  }
+];
+
 
 export default function Dashboard() {
   return (
@@ -15,6 +64,36 @@ export default function Dashboard() {
 
       <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
         <Sidebar />
+
+        <SimpleGrid
+          flex="1"
+          gap="4"
+          minChildWidth="320px" //Esta propriedade só existe dentro do SimpleGrid
+        >
+          <Box
+            p="8"
+            bg="gray.800"
+            borderRadius="8"
+            pb="4"
+          >
+            <Text fontSize="lg" mb="4">Inscritos da semana</Text>
+            <Chart
+              options={options}
+              series={series}
+              type="area"
+              height={160}
+            />
+          </Box>
+
+          <Box
+            p="8"
+            bg="gray.800"
+            borderRadius="8"
+            // pb="4"
+          >
+            <Text fontSize="lg" mb="4">Taxa de abertura</Text>
+          </Box>
+        </SimpleGrid>
       </Flex>
     </Flex>
   )
