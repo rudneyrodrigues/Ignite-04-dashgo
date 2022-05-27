b4301e60d0339d25a146ffeb795da28839154f04
Rudney R <rudney.un2016@gmail.com>
2022-05-26 22:33:52 -0300

    Authentication form

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,19 @@
+import { Button, Flex, Stack } from '@chakra-ui/react';
+import { FieldValues, SubmitHandler, useForm } from 'react-hook-form';
 import type { NextPage } from 'next';
 import Head from 'next/head';
-import { Button, Flex, Stack } from '@chakra-ui/react';
 
 import { Input } from '../components/Form/Input';
 
 const Home: NextPage = () => {
+  const { register, handleSubmit, formState } = useForm();
+
+  const handleSignIn: SubmitHandler<FieldValues> = async (values, event) => {
+    await new Promise(resolve => setTimeout(resolve, 2000));
+    
+    console.log(values);
+  }
+
   return (
     <>
       <Head>
@@ -25,13 +34,32 @@ const Home: NextPage = () => {
           bg="gray.800"
           p="8"
           borderRadius=".5rem"
+          onSubmit={handleSubmit(handleSignIn)}
         >
           <Stack spacing={4}>
-            <Input name='email' label='E-mail' type="email" />
-            <Input name='password' label='Senha' type="password" />
+            <Input
+              // name='email'
+              label='E-mail'
+              type="email"
+              {...register('email')}
+            />
+            <Input
+              // name='password'
+              label='Senha'
+              type="password"
+              {...register('password')}
+            />
           </Stack>
 
-          <Button type='submit' mt="6" colorScheme="pink" size="lg">Entrar</Button>
+          <Button
+            type='submit'
+            mt="6"
+            colorScheme="pink"
+            size="lg"
+            isLoading={formState.isSubmitting}
+          >
+            Entrar
+          </Button>
         </Flex>
       </Flex>
     </>
